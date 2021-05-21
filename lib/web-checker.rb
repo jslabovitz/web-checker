addc1146d442687fa6ba9ec6b24fbeef4cb0cae1
John Labovitz <johnl@johnlabovitz.com>
2021-05-21 10:49:47 -0400

    Various:
    - Fix parsing.
    - Remove use of 'tidy', since it doesn't handle HTML5.
    - Refactor/rework to be simpler.
    - Get remote resources, not just local.

--- lib/web-checker.rb
+++ lib/web-checker.rb
@@ -6,21 +6,10 @@ require 'path'
 
 class WebChecker
 
-  IgnoreErrors = %Q{
-    <table> lacks "summary" attribute
-    <img> lacks "alt" attribute
-    <form> proprietary attribute "novalidate"
-    <input> attribute "type" has invalid value "email"
-    <input> attribute "tabindex" has invalid value "-1"
-    <input> proprietary attribute "border"
-    trimming empty <p>
-    <iframe> proprietary attribute "allowfullscreen"
-  }.split(/\n/).map(&:strip)
-  LinkElementsXPath = '//@href | //@src'
   SchemasDir = Path.new(__FILE__).dirname / 'web-checker' / 'schemas'
-  Schemas = {
-    'feed' => SchemasDir / 'atom.xsd',
-    'urlset' => SchemasDir / 'sitemap.xsd',
+  SchemaNames = {
+    'feed' => 'atom',
+    'urlset' => 'sitemap',
   }
 
   class Error < Exception; end
@@ -29,7 +18,8 @@ class WebChecker
     @site_uri = Addressable::URI.parse(site_uri)
     @site_dir = Path.new(site_dir)
     @schemas = {}
-    @visited = {}
+    @seen = {}
+    @files = []
   end
 
   def check
@@ -41,26 +31,27 @@ class WebChecker
   def check_uri(uri)
     uri = Addressable::URI.parse(uri)
     uri.normalize!
-    return unless local?(uri) && !seen?(uri)
-    # ;;warn "CHECKING: #{uri}"
+    return if seen?(uri)
+    return unless http?(uri)
+    is_local = local?(uri)
+    ;;warn "CHECKING: #{uri}"
     response = HTTP.get(uri)
     # ;;pp(response: response)
-    @visited[uri] = true
+    @seen[uri] = true
     case response.code
     when 200...300
-      body = response.body.to_s
-      # ;;pp(body: body)
-      case (type = response.headers['Content-Type'])
-      when 'text/html'
-        check_html(uri, body)
-      when 'text/css'
-        check_css(uri, body)
-      when 'application/xml', 'text/xml'
-        check_xml(uri, body)
-      when 'image/jpeg', 'image/png', 'image/gif', 'application/javascript'
-        # ignore
-      else
-        ;;warn "skipping unknown resource type: #{uri} (#{type})"
+      if is_local
+        data = response.body.to_s
+        case (type = response.headers['Content-Type'])
+        when 'text/html', 'text/xml', 'application/xml'
+          check_markup(uri, data)
+        when 'text/css'
+          check_css(uri, data)
+        when %r{^image/}, 'application/javascript'
+          # ignore
+        else
+          ;;warn "skipping unknown resource type: #{uri} (#{type})"
+        end
       end
     when 300...400
       redirect_uri = Addressable::URI.parse(response.headers['Location'])
@@ -72,60 +63,32 @@ class WebChecker
     end
   end
 
-  def check_html(uri, html)
-    check_html_tidy(uri, html)
-    check_html_nokogiri(uri, html)
-  end
-
-  def check_html_tidy(uri, html)
-    tmp_file = Path.tmpfile
-    tmp_file.write(html)
-    errors = %x{tidy -utf8 -quiet -errors #{tmp_file} 2>&1}.split("\n")
-    errors = errors.map { |str|
-      # line 82 column 1 - Warning: <table> lacks "summary" attribute
-      str =~ /^line (\d+) column (\d+) - (.*?): (.*)$/ or raise "Can't parse error: #{str.inspect}"
-      {
-        msg: str,
-        line: $1.to_i,
-        column: $2.to_i,
-        type: $3.downcase.to_sym,
-        error: $4.strip,
-      }
-    }.reject { |e|
-      IgnoreErrors.include?(e[:error])
-    }
-    unless errors.empty?
-      warn "#{uri} has invalid HTML"
-      show_errors(errors)
-      raise Error, "HTML parsing failed (via Tidy)"
+  def check_markup(uri, data)
+# ;;warn "validating markup: #{uri}"
+    doc = case data
+    when /^<\?xml/i
+      Nokogiri::XML(data) { |c| c.strict }
+    when /^<!DOCTYPE html>/i
+      Nokogiri::HTML5(data, max_errors: -1)
+    else
+      Nokogiri::HTML(data) { |c| c.strict }
     end
-  end
-
-  def check_html_nokogiri(uri, html)
-    doc_class = (html =~ /<!DOCTYPE html>/i) ? Nokogiri::HTML5 : Nokogiri::HTML
-    doc = doc_class.parse(html) { |config| config.strict }
     unless doc.errors.empty?
       show_errors(doc.errors)
-      raise Error, "HTML parsing failed (via Nokogiri)"
+      raise Error, "markup parsing failed"
     end
-    doc.xpath(LinkElementsXPath).each { |e| check_uri(uri + e.value) }
-  end
-
-  def check_xml(uri, xml)
-    xml_doc = Nokogiri::XML::Document.parse(xml) { |config| config.strict }
-    unless xml_doc.errors.empty?
-      show_errors(xml_doc.errors)
-      raise Error, "XML parsing failed"
+    if (schema_name = SchemaNames[doc.root.name])
+      schema_file = (SchemasDir / schema_name).add_extension('.xsd')
+      schema = (@schemas[schema_file] ||= Nokogiri::XML::Schema(schema_file.open))
+      validation_errors = schema.validate(doc)
+      unless validation_errors.empty?
+        show_errors(validation_errors)
+        raise Error, "schema validation failed"
+      end
     end
-    root_name = xml_doc.root.name
-    schema_file = Schemas[root_name] or raise Error, "Unknown schema: #{root_name.inspect}"
-    schema = (@schemas[schema_file] ||= Nokogiri::XML::Schema(schema_file.open))
-    validation_errors = schema.validate(xml_doc)
-    unless validation_errors.empty?
-      show_errors(validation_errors)
-      raise Error, "XML validation failed"
+    doc.xpath('//@href | //@src').each do |elem|
+      check_uri(uri + elem.value)
     end
-    xml_doc.xpath(LinkElementsXPath).each { |e| check_uri(uri + e.value) }
   end
 
   def show_errors(errors)
@@ -140,13 +103,17 @@ class WebChecker
     end
   end
 
+  def http?(uri)
+    !uri.scheme || %w[http https].include?(uri.scheme)
+  end
+
   def local?(uri)
     (!uri.scheme && !uri.host) ||
       (uri.scheme == @site_uri.scheme && uri.host == @site_uri.host && uri.port == @site_uri.port)
   end
 
   def seen?(uri)
-    @visited[uri]
+    @seen[uri]
   end
 
   def report
